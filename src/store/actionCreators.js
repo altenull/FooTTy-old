aa3a9f57ac989f7e674689bcfe93153a1daa2cbc
altenull <altenull@gmail.com>
2018-02-08 00:58:24 +0900

    added LeagueActionCreators

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -2,7 +2,9 @@
 import { bindActionCreators } from 'redux';
 import store from './index';
 import { actionCreators as baseActions, type BaseActionCreators } from './modules/base';
+import { actionCreators as leagueActions, type LeagueActionCreators } from './modules/league';
 
 const { dispatch } = store;
 
 export const BaseActions: BaseActionCreators = bindActionCreators(baseActions, dispatch);
+export const LeagueActions: LeagueActionCreators = bindActionCreators(leagueActions, dispatch);

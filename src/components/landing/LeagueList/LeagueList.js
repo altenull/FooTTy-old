5640b921439e19997acada3fdb997ce6774f5a5a
altenull <altenull@gmail.com>
2018-02-07 16:06:07 +0900

    implemented LeagueList component

---
+++ src/components/landing/LeagueList/LeagueList.js
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react';
+import styles from './LeagueList.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const LeagueList = () => {
+  const premierLeagueImg = require('static/images/PremierLeague_Badge_Gray.png');
+  const laLigaImg = require('static/images/LaLiga_Badge_Gray.png');
+  const bundesLigaImg = require('static/images/BundesLiga_Badge_Gray.png');
+
+  return (
+    <div className={cx('league-list')}>
+      <div className={cx('league', 'fade-enter1')}>
+        <img src={premierLeagueImg} className={cx('premierleague')} alt='premier league' />
+        <span>PREMIER LEAGUE</span>
+      </div>
+      <div className={cx('league', 'fade-enter2')}>
+        <img src={laLigaImg} className={cx('laliga')} alt='la liga' />
+        <span>LA LIGA</span>
+      </div>
+      <div className={cx('league', 'fade-enter3')}>
+        <img src={bundesLigaImg} className={cx('bundesliga')} alt='bundesliga' />
+        <span>BUNDESLIGA</span>
+      </div>
+    </div>
+  );
+}
+
+export default LeagueList;

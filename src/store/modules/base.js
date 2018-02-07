e5e72756062cc714107e15c7205ffc4e85ce6799
altenull <altenull@gmail.com>
2018-02-08 00:56:41 +0900

    added INITIALIZE_BASE_STATE action

--- src/store/modules/base.js
+++ src/store/modules/base.js
@@ -1,18 +1,22 @@
 // @flow
 import { createAction, handleActions } from 'redux-actions';
 import { Record, type Map } from 'immutable';
+import { pender } from 'redux-pender';
 
 // action types
 const START_LANDING = 'base/START_LANDING';
+const INITIALIZE_BASE_STATE = 'base/INITIALIZE_BASE_STATE';
 const CHANGE_CURRENT_PAGE = 'base/CHANGE_CURRENT_PAGE';
 
 export type BaseActionCreators = {
   startLanding(): any,
+  initializeBaseState(): any,
   changeCurrentPage(currentPage: string): any
 };
 
 export const actionCreators = {
   startLanding: createAction(START_LANDING),
+  initializeBaseState: createAction(INITIALIZE_BASE_STATE),
   changeCurrentPage: createAction(CHANGE_CURRENT_PAGE)
 };
 
@@ -30,5 +34,6 @@ const initialState: Map<string, *> = BaseRecord();
 
 export default handleActions({
   [START_LANDING]: state => state.set('landing', true),
+  [INITIALIZE_BASE_STATE]: state => initialState,
   [CHANGE_CURRENT_PAGE]: (state, { payload: currentPage }) => state.set('currentPage', currentPage)
 }, initialState);

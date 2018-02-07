f88b0b0ff90188da45bdc091a83ad4bd77213f41
altenull <altenull@gmail.com>
2018-02-08 00:55:44 +0900

    added League State

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 // @flow
 import configure from './configure';
 import type { Base } from './modules/base';
+import type { League } from './modules/league';
 
 const store = configure();
 
@@ -8,6 +9,7 @@ export default store;
 
 export type State = {
   base: Base,
+  league: League,
   pender: {
     pending: any,
     success: any,

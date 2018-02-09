e93c369e79c93946b65c9efda6828106d3932ec5
altenull <altenull@gmail.com>
2018-02-09 23:59:10 +0900

    implemented league api

---
+++ src/lib/api/league.js
@@ -0,0 +1,11 @@
+// @flow
+import axios from 'axios';
+
+export const getLatestSeason = (leagueId: string): Promise<*> => axios.get(`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${leagueId}&s=all`);
+
+type getLeagueTablePayload = {
+  leagueId: string,
+  latestSeason: string
+};
+
+export const getLeagueTable = ({ leagueId, latestSeason }: getLeagueTablePayload): Promise<*> => axios.get(`https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=${leagueId}&s=${latestSeason}`);

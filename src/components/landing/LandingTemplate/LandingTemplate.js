b573312dbb9eb24bd3a65bd06d52318f98ce1a30
altenull <altenull@gmail.com>
2018-02-09 23:58:29 +0900

    moved landing state to inner state of LandingTemplate

--- src/components/landing/LandingTemplate/LandingTemplate.js
+++ src/components/landing/LandingTemplate/LandingTemplate.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { type Node } from 'react';
+import React, { Component, type Node } from 'react';
 import styles from './LandingTemplate.scss';
 import classNames from 'classnames/bind';
 import ReactRevealText from 'react-reveal-text/lib';
@@ -7,39 +7,55 @@ import ReactRevealText from 'react-reveal-text/lib';
 const cx = classNames.bind(styles);
 
 type Props = {
-  landing: boolean,
   leagueList: Node,
   footer: Node
 };
 
-const LandingTemplate = ({ landing, leagueList, footer }: Props) => {
-  return (
-    <div>
-      <div className={cx('landing-template')}>
-        <div className={cx('left-wrapper')}>
-          <h1>
-            <ReactRevealText
-              show={landing}
-              text='FOOTTY' />
-          </h1>
-          <h2>
-            <ReactRevealText
-              show={landing}
-              text='A FOOTBALL TERMINAL' />
-          </h2>
-          <h3>
-            <ReactRevealText
-              show={landing}
-              text='SPORTS &nbsp;&nbsp;X&nbsp;&nbsp; TECHNOLOGY' />
-          </h3>
+class LandingTemplate extends Component<Props> {
+  state = {
+    show: false
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        show: true
+      });
+    }, 200);
+  }
+
+  render() {
+    const { leagueList, footer } = this.props;
+    const { show } = this.state;
+
+    return (
+      <div>
+        <div className={cx('landing-template')}>
+          <div className={cx('left-wrapper')}>
+            <h1>
+              <ReactRevealText
+                show={show}
+                text='FOOTTY' />
+            </h1>
+            <h2>
+              <ReactRevealText
+                show={show}
+                text='A FOOTBALL TERMINAL' />
+            </h2>
+            <h3>
+              <ReactRevealText
+                show={show}
+                text='SPORTS &nbsp;&nbsp;X&nbsp;&nbsp; TECHNOLOGY' />
+            </h3>
+          </div>
+          <div className={cx('right-wrapper')}>
+            {leagueList}
+          </div>
         </div>
-        <div className={cx('right-wrapper')}>
-          {leagueList}
-        </div>
+        {footer}
       </div>
-      {footer}
-    </div>
-  );
+    );
+  }
 }
 
 export default LandingTemplate;

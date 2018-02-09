8e374c447a261ff2b316cfe6bd5aaccaf5bd8ccb
altenull <altenull@gmail.com>
2018-02-09 23:59:27 +0900

    implemented leagueInfo variables

---
+++ src/lib/variables.js
@@ -0,0 +1,32 @@
+// exports.leagueInfo = {
+//   'PL': {
+//     id: 4328,
+//     name: 'English Premier League'
+//   },
+//   'LL': {
+//     id: 4335,
+//     name: 'Spanish La Liga'
+//   },
+//   'BL': {
+//     id: 4331,
+//     name: 'German Bundesliga'
+//   }
+// }
+
+export const leagueInfo = [
+  {
+    alias: 'PL',
+    id: 4328,
+    name: 'English Premier League'
+  },
+  {
+    alias: 'LL',
+    id: 4335,
+    name: 'Spanish La Liga'
+  },
+  {
+    alias: 'BL',
+    id: 4331,
+    name: 'German Bundesliga'
+  }
+];

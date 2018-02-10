6ecb856d1d13c52f3907147de917829dabe073be
altenull <altenull@gmail.com>
2018-02-11 02:27:50 +0900

    implemented HeaderWrapper component

---
+++ src/components/common/HeaderWrapper/HeaderWrapper.js
@@ -0,0 +1,23 @@
+// @flow
+import React from 'react';
+import type { Node } from 'react';
+import styles from './HeaderWrapper.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+type Props = {
+  children: Node
+};
+
+const HeaderWrapper = ({children}: Props) => {
+  return (
+    <div className={cx('container')}>
+      <div className={cx('header-wrapper')}>
+        {children}
+      </div>
+  </div>
+  );
+}
+
+export default HeaderWrapper;

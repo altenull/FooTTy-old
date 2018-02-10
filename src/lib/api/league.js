8ba54a6776cec2b512264c589bb554bdca1609a9
altenull <altenull@gmail.com>
2018-02-11 02:24:37 +0900

    implemented league actions

--- src/lib/api/league.js
+++ src/lib/api/league.js
@@ -9,3 +9,6 @@ type getLeagueTablePayload = {
 };
 
 export const getLeagueTable = ({ leagueId, latestSeason }: getLeagueTablePayload): Promise<*> => axios.get(`https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=${leagueId}&s=${latestSeason}`);
+
+// leagues배열 안에 strWebsite, strFacebook, strTwitter, strYoutube, strBadge(imgUrl)
+export const getLeagueDetails = (leagueId: string): Promise<*> => axios.get(`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${leagueId}`);

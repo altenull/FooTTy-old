17ea634879d22f17dfd36cef7d5e1a12d3a0dbc6
altenull <altenull@gmail.com>
2018-02-11 02:27:13 +0900

    implemented league list select logic with LeagueActions.getLeagueTable function

--- src/containers/landing/LandingTemplateContainer.js
+++ src/containers/landing/LandingTemplateContainer.js
@@ -2,42 +2,70 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import type { State } from 'store';
-import { BaseActions } from 'store/actionCreators';
+import type { LatestSeasons } from 'store/modules/league';
+import { BaseActions, LeagueActions } from 'store/actionCreators';
+import { leagueInfo } from 'lib/variables';
 import LandingTemplate from 'components/landing/LandingTemplate';
 import LeagueList from 'components/landing/LeagueList';
 import Footer from 'components/landing/Footer';
 
 type Props = {
-  landing: boolean
+  latestSeasons: ?LatestSeasons
 };
 
 class LandingTemplateContainer extends Component<Props> {
   componentDidMount() {
-    setTimeout(() => {
-      BaseActions.startLanding();
-    }, 200);
+    BaseActions.initializeBaseState();
+  }
+
+  handleClick = async (leagueId: string) => {
+    const { latestSeasons } = this.props;
+    let latestSeason = null;
+    let currentLeague = null;
+
+    if (leagueId === leagueInfo.find(c => c.alias === 'PL').id) {
+      latestSeason = latestSeasons.latestSeasonPL;
+      currentLeague = 'PL';
+    } else if (leagueId === leagueInfo.find(c => c.alias === 'LL').id) {
+      latestSeason = latestSeasons.latestSeasonLL;
+      currentLeague = 'LL';
+    } else if (leagueId === leagueInfo.find(c => c.alias === 'BL').id) {
+      latestSeason = latestSeasons.latestSeasonLL;
+      currentLeague = 'BL';
+    }
+
+    try {
+      await LeagueActions.getLeagueTable({ leagueId, latestSeason });
+      BaseActions.changeCurrentPage('league');
+      LeagueActions.setCurrentLeague(currentLeague);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
-    const { landing } = this.props;
+    const { handleClick } = this;
+    const leagueList = (
+      <LeagueList
+        onClick={handleClick}
+      />
+    );
+    const footer = (
+      <Footer />
+    );
 
     return (
       <LandingTemplate
-        landing={landing}
-        leagueList={
-          <LeagueList />
-        }
-        footer={
-          <Footer />
-        }
+        leagueList={leagueList}
+        footer={footer}
       />
     );
   }
 }
 
 export default connect(
-  ({ base }: State) => ({
-    landing: base.landing
+  ({ league }: State) => ({
+    latestSeasons: league.latestSeasons
   }),
   () => ({})
 )(LandingTemplateContainer);

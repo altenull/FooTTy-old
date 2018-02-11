8085c3e51bea7681a633ca83e3507f8cec345501
altenull <altenull@gmail.com>
2018-02-11 20:51:34 +0900

    deleted latestSeasons set function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+// @flow
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import type { State } from 'store';
 import PageTemplate from 'components/base/PageTemplate';
 import LandingTemplateContainer from 'containers/landing/LandingTemplateContainer';
+import LeagueTemplateContainer from 'containers/league/LeagueTemplateContainer';
 
-class App extends Component {
+type Props = {
+  currentPage: string
+};
+
+class App extends Component<Props> {
+  renderTemplate = (currentPage: string) => {
+    switch (currentPage) {
+    case 'home':
+      return <LandingTemplateContainer />;
+    case 'league':
+      return <LeagueTemplateContainer />;
+    default:
+      return null;
+    }
+  }
+
   render() {
+    const { currentPage } = this.props;
+    const { renderTemplate } = this;
+
     return (
       <PageTemplate>
-        <LandingTemplateContainer />
+        {renderTemplate(currentPage)}
       </PageTemplate>
     );
   }
 }
 
-export default App;
+export default connect(
+  ({ base }: State) => ({
+    currentPage: base.currentPage
+  }),
+  () => ({})
+)(App);

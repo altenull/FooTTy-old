c8c69089299f6a5b333dff7629f6bb2a6b20b943
altenull <altenull@gmail.com>
2018-02-12 23:06:59 +0900

    changed images name and added variables

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,32 +1,29 @@
-// exports.leagueInfo = {
-//   'PL': {
-//     id: 4328,
-//     name: 'English Premier League'
-//   },
-//   'LL': {
-//     id: 4335,
-//     name: 'Spanish La Liga'
-//   },
-//   'BL': {
-//     id: 4331,
-//     name: 'German Bundesliga'
-//   }
-// }
-
 export const leagueInfo = [
   {
     alias: 'PL',
     id: 4328,
-    name: 'English Premier League'
+    name: 'Premier League',
+    fullname: 'English Premier League',
+    country: 'United Kingdom',
+    badgeImg: 'PL_badge.png',
+    flagImg: 'PL_flag.png'
   },
   {
     alias: 'LL',
     id: 4335,
-    name: 'Spanish La Liga'
+    name: 'La Liga',
+    fullname: 'Spanish La Liga',
+    country: 'Spain',
+    badgeImg: 'LL_badge.png',
+    flagImg: 'LL_flag.png'
   },
   {
     alias: 'BL',
     id: 4331,
-    name: 'German Bundesliga'
+    name: 'Bundesliga',
+    fullname: 'German Bundesliga',
+    country: 'Germany',
+    badgeImg: 'BL_badge.png',
+    flagImg: 'BL_flag.png'
   }
 ];

43b230ca526b51ef6674278f3d4f57b3cc02b883
altenull <altenull@gmail.com>
2018-02-12 23:15:46 +0900

    optimized league Header component and added flag img

--- src/components/league/Header/Header.js
+++ src/components/league/Header/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
 import HeaderWrapper from 'components/common/HeaderWrapper';
+import { leagueInfo } from 'lib/variables.js';
 
 const cx = classNames.bind(styles);
 
@@ -11,27 +12,27 @@ type Props = {
 };
 
 const Header = ({currentLeague}: Props) => {
-  let leagueBadgeImg = null;
-  let leagueName = '';
+  const findResult = leagueInfo.find(c => c.alias === currentLeague);
 
-  if (currentLeague === 'PL') {
-    leagueBadgeImg = require('static/images/PremierLeague_Badge.png');
-    leagueName = 'Premier League';
-  } else if (currentLeague === 'LL') {
-    leagueBadgeImg = require('static/images/LaLiga_Badge.png');
-    leagueName = 'La Liga';
-  } else if (currentLeague === 'BL') {
-    leagueBadgeImg = require('static/images/BundesLiga_Badge.png');
-    leagueName = 'Bundesliga';
-  }
+  const badgeImgURL = require(`static/images/${findResult.badgeImg}`);
+  const flagImgURL = require(`static/images/${findResult.flagImg}`);
+  const leagueName = findResult.name;
+  const flagAlt = findResult.country;
 
   return (
     <HeaderWrapper>
       <div className={cx('header', 'slide-enter')}>
-        <img src={leagueBadgeImg} alt={leagueName} />
-        <h1>
-          {leagueName}
-        </h1>
+        <div className={cx('logo')}>
+          <img src={badgeImgURL} alt={leagueName} />
+          <h1>
+            {leagueName}
+          </h1>
+        </div>
+        <div className={cx('spacer')}>
+        </div>
+        <div className={cx('flag')}>
+          <img src={flagImgURL} alt={flagAlt} />
+        </div>
       </div>
     </HeaderWrapper>
   );

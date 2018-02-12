5a2ada2875c3a341038e7870114632743da80c9d
altenull <altenull@gmail.com>
2018-02-12 23:09:05 +0900

    changed img variables name

--- src/components/landing/LeagueList/LeagueList.js
+++ src/components/landing/LeagueList/LeagueList.js
@@ -10,22 +10,22 @@ type Props = {
 };
 
 const LeagueList = ({onClick}: Props) => {
-  const premierLeagueImg = require('static/images/PremierLeague_Badge_Gray.png');
-  const laLigaImg = require('static/images/LaLiga_Badge_Gray.png');
-  const bundesLigaImg = require('static/images/BundesLiga_Badge_Gray.png');
+  const PLbadgeGrayImgURL = require('static/images/PL_badge_g.png');
+  const LLbadgeGrayImgURL = require('static/images/LL_badge_g.png');
+  const BLbadgeGrayImgURL = require('static/images/BL_badge_g.png');
 
   return (
     <div className={cx('league-list')}>
       <div className={cx('league', 'fade-enter1')} onClick={() => onClick('PL')}>
-        <img src={premierLeagueImg} className={cx('premierleague')} alt='premier league' />
+        <img src={PLbadgeGrayImgURL} className={cx('premierleague')} alt='Premier League' />
         <span>PREMIER LEAGUE</span>
       </div>
       <div className={cx('league', 'fade-enter2')} onClick={() => onClick('LL')}>
-        <img src={laLigaImg} className={cx('laliga')} alt='la liga' />
+        <img src={LLbadgeGrayImgURL} className={cx('laliga')} alt='La Liga' />
         <span>LA LIGA</span>
       </div>
       <div className={cx('league', 'fade-enter3')} onClick={() => onClick('BL')}>
-        <img src={bundesLigaImg} className={cx('bundesliga')} alt='bundesliga' />
+        <img src={BLbadgeGrayImgURL} className={cx('bundesliga')} alt='Bundesliga' />
         <span>BUNDESLIGA</span>
       </div>
     </div>

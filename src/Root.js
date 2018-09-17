103899f5c66b613c953bf73c983c209f0e59262f
Heonyoung Kim <altenull@gmail.com>
2018-09-17 23:52:50 +0900

    chore: seperated start scripts (#4)

--- src/Root.js
+++ src/Root.js
@@ -1,18 +1,18 @@
 // @flow
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
-import ReactGA from 'react-ga';
+import * as ReactGA from 'react-ga';
 
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 
 const Root = () => (
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
